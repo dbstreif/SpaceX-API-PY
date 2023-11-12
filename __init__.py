c53e25bea92c11324b086da0e8062a0bfa19ba57
Dom13377 <streif.dominic@gmail.com>
2023-11-11 19:07:20 -0800

    changed init request url

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ from requests import head
 def init() -> None:
     """Checks for SpaceX API heartbeat"""
 
-    alive = head("https://api.spacexdata.com/v4/capsules")
+    alive = head("https://api.spacexdata.com/")
     if alive.status_code != 200:
         raise Exception("Cannot connect to SpaceX Capsule API!")
