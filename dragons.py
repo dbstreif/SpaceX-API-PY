0eddea08a1a4be86e399b333e4ab22fbcd06274f
Dom13377 <geegne123@gmail.com>
2023-11-22 19:40:04 -0800

    push

--- dragons.py
+++ dragons.py
@@ -8,9 +8,24 @@ def get_all_dragons() -> dict:
     jsondata = get("https://api.spacexdata.com/v4/dragons").json()
     return jsondata
 
-def get_crew_member(dragon_id: str) -> dict:
+def get_dragon(dragon_id: str) -> dict:
     """Retreives information of specified SpaceX dragon"""
 
     init()
     jsondata = get(f"https://api.spacexdata.com/v4/dragons/:{dragon_id}").json()
     return jsondata
+
+def get_dragon_id() -> list[dict]:
+    """Retreives the id and serial number of all current dragons"""
+
+    init()
+    jsondata = get("https://api.spacexdata.com/v4/dragons").json()
+    idseriallist = []
+
+    for data in jsondata:
+        serialgrouping = {}
+        serialgrouping["id"] = data["id"]
+        serialgrouping["name"] = data["name"]
+        idseriallist.append(serialgrouping)
+    
+    return idseriallist
